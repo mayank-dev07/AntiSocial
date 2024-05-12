de6b9442b5d197d586baf77f275898775c6a64bd
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-12 23:42:29 +0530

    url changed

--- front-end/src/axios/axiosInstance.js
+++ front-end/src/axios/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:8000/api/";
+export const url = "http://192.168.10.59:8000/api/";
 
 const axiosInstance = axios.create({
   baseURL: url,
--- front-end/src/axios/imageurl.js
+++ front-end/src/axios/imageurl.js
@@ -1,1 +1,1 @@
-export const url = "http://localhost:8000/";
+export const url = "http://192.168.10.59:8000/";
