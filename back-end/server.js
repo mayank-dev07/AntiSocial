fb01b7522eb62e24bb838d497caf8c12dd4975b3
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-12 21:15:14 +0530

    error

--- back-end/server.js
+++ back-end/server.js
@@ -35,6 +35,18 @@ app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/message", messageRouter);
 
+// Add CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", corsOptions.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 server.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
