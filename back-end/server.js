aaba97379a194eed32e6a36da61fcba7ee94291d
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-12 23:10:31 +0530

    error

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,3 @@
-// server.js
 import express from "express";
 import dotenv from "dotenv";
 import connetDB from "./db/connectDB.js";
@@ -13,19 +12,17 @@ dotenv.config();
 connetDB();
 
 const PORT = process.env.PORT || 8000;
-
 const corsOptions = {
   origin: "http://localhost:3000",
-  credentials: true,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // to parse json data in to req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data in the req body
 app.use(cookieParser());
 app.use(cors(corsOptions));
-
-// Static file serving
 app.use("/uploads/profileimg", express.static("uploads/profileimg"));
 app.use("/uploads/postimg", express.static("uploads/postimg"));
 app.use("/uploads/messageimg", express.static("uploads/messageimg"));
