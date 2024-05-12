e6fbd7048a2fff2396e38655d32090de7497e711
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-12 20:48:07 +0530

    error

--- front-end/src/axios/axiosInstance.js
+++ front-end/src/axios/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:8000/api/";
+export const url = "http://anti-social-two.vercel.app/api/";
 
 const axiosInstance = axios.create({
   baseURL: url,
