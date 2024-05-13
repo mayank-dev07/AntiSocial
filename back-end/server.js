54861b1373a00883a946b88c234cb5cb28ad0ec6
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-13 19:40:12 +0530

    deploy test 25

--- back-end/server.js
+++ back-end/server.js
@@ -13,7 +13,7 @@ connetDB();
 
 const PORT = process.env.PORT || 8000;
 const corsOptions = {
-  origin: "https://anti-social-frontend.vercel.app/",
+  origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
