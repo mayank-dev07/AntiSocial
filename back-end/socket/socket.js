6e5e4cecdc26e350112e98354f4a6076de2b90d4
mayank-dev07 <devmayank.hts@gmail.com>
2024-05-13 19:50:08 +0530

    deploy test 30

--- back-end/socket/socket.js
+++ back-end/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: true,
+    origin: "*",
     credentials: true,
   },
   allowEIO3: true,
